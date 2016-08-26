6e549ffe90ebbe78799da7dfba0f59256be6634a
Jean Fabre <contact@fabrejean.net>
2016-08-26 14:04:40 +0300

    - Cleaned up code and comments - Added ColorPerPlayer system - Added watermark

--- Assets/PUN Custom Samples/SmartPool/Scripts/PunSmartPoolBridge.cs
+++ Assets/PUN Custom Samples/SmartPool/Scripts/PunSmartPoolBridge.cs
@@ -5,8 +5,8 @@ using System.Collections;
 /// <summary>
 /// Pun smart pool bridge.
 /// Common Pitfalls:
-/// -- even when using a pool managerm you need to store your prefab inside a Resources Folder, 
-///    it's because PUN needs to load the prefab and assign viewIDs to all PhotonViews. Note, It's not instantiated, just loaded for analyzing.
+/// -- even when using a pool manager, you need to store your prefab inside a Resources Folder, 
+///    it's because PUN needs to load the prefab and assign viewIDs to all PhotonViews. Note: It's not instantiated by pun at all, just loaded for analyzing.
 /// -- on the instanciated prefab, use OnPhotonInstantiate() to catch info on initializating if you must, OnEnable and Start aren't suitable due to the network initializaion processes.
 /// </summary>
 public class PunSmartPoolBridge : MonoBehaviour, IPunPrefabPool
@@ -16,15 +16,9 @@ public class PunSmartPoolBridge : MonoBehaviour, IPunPrefabPool
 	    PhotonNetwork.PrefabPool = this;
 	}
 
-    public void OnGUI()
-    {
-        GUILayout.Label("photonViewList " + PhotonNetwork.networkingPeer.photonViewList.Count);
-    }
-
-
     public GameObject Instantiate(string prefabId, Vector3 position, Quaternion rotation)
     {
-        Debug.LogWarning("Prefab: " + prefabId);
+		Debug.LogWarning("Instantiate Prefab: " + prefabId);
 		
 		GameObject go = SmartPool.Spawn(prefabId);
         go.transform.position = position;
--- Assets/PUN Custom Samples/SmartPool/Scripts/PunSmartPoolDemo.cs
+++ Assets/PUN Custom Samples/SmartPool/Scripts/PunSmartPoolDemo.cs
@@ -3,19 +3,12 @@ using System.Collections;
 
 using ExitGames.Client.Photon;
 
+/// <summary>
+/// Pun smart pool demo. Simple script to instantiate the player. Note that this script doesn't change whether you use a pool manager or not.
+/// Check out PunSmartPoolBridge.cs to see how pooling is enabled.
+/// </summary>
 public class PunSmartPoolDemo: Photon.PunBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-
 	public override void OnJoinedRoom ()
 	{
 		PhotonNetwork.Instantiate("SmartPoolPlayer", new Vector3(0f,0f,0f), Quaternion.identity, 0);
